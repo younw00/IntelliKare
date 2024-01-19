a76453610b2d7064ca9078f220207d890cfbe7a8
younwoo <younwoo@umich.edu>
2024-01-19 16:08:03 -0500

    first commit

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void Scene1()
+    {
+        Debug.Log("1 is pressed");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Scene2()
+    {
+        Debug.Log("2 is pressed");
+        SceneManager.LoadScene(2);
+    }
+
+    public void Scene3()
+    {
+        Debug.Log("3 is pressed");
+        SceneManager.LoadScene(3);
+    }
+
+    public void Scene4()
+    {
+        Debug.Log("4 is pressed");
+        SceneManager.LoadScene(4);
+    }
+
+    public void Scene5()
+    {
+        Debug.Log("5 is pressed");
+        SceneManager.LoadScene(5);
+    }
+
+    public void Scene6()
+    {
+        Debug.Log("6 is pressed");
+        SceneManager.LoadScene(6);
+    }
+
+    public void Scene7()
+    {
+        Debug.Log("7 is pressed");
+        SceneManager.LoadScene(7);
+    }
+
+    public void Scene8()
+    {
+        Debug.Log("8 is pressed");
+        SceneManager.LoadScene(8);
+    }
+
+    public void Scene9()
+    {
+        Debug.Log("9 is pressed");
+        SceneManager.LoadScene(9);
+    }
+}
